4528da6a35715b642f39370930e0cb8e464e1f93
Mario Murrent <mario@murrent.at>
2023-08-10 13:40:10 +0200

    chore: fixed types and method calling

--- packages/did-resolver-oyd/src/__tests__/oyd_resolver.test.ts
+++ packages/did-resolver-oyd/src/__tests__/oyd_resolver.test.ts
@@ -1,11 +1,10 @@
-import { DID_JSON, DID_LD_JSON, resolveDidOyd } from '../index'
-import * as fs from 'fs'
+import { resolveDidOyd } from '../index';
+import * as fs from 'fs';
 
 describe('@ownyourdata/oyd-did-resolver', () => {
   it('should resolve did:oyd', async () => {
     const doc = await resolveDidOyd(
-      'did:oyd:zQmaBZTghndXTgxNwfbdpVLWdFf6faYE4oeuN2zzXdQt1kh', undefined, undefined, undefined
-    )
-    expect(doc).toEqual(JSON.parse(fs.readFileSync(`${__dirname}/fixtures/did_oyd.jsonld`, { encoding: 'utf-8' })))
-  })
-})
+      'did:oyd:zQmaBZTghndXTgxNwfbdpVLWdFf6faYE4oeuN2zzXdQt1kh');
+    expect(doc).toEqual(JSON.parse(fs.readFileSync(`${__dirname}/fixtures/did_oyd.jsonld`, { encoding: 'utf-8' })));
+  });
+});
